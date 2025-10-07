c8bcd75450f3a3e8d8dd1901b96c37c922e6b947
JBahulika <Jbahulika@gmail.com>
2025-10-07 17:58:22 +0530

    -

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,14 @@
+
 // next.config.ts
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  reactStrictMode: true
+  webpack: (config) => {
+    // Prevent pdfjs-dist from trying to resolve native 'canvas'
+    config.externals.push({ canvas: 'canvas' });
+    return config;
+  },
 };
 
 export default nextConfig;
+
